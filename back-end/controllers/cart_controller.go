68833714cf77acbe97561143f14f9ffddcf27ace
ReynoBenedict <143096192+ReynoBenedict@users.noreply.github.com>
2025-04-13 09:09:41 +0700

    update minor backend cart

--- back-end/controllers/cart_controller.go
+++ back-end/controllers/cart_controller.go
@@ -55,7 +55,7 @@ func UpdateCartItem(c *gin.Context) {
 
 func DeleteCartItem(c *gin.Context) {
 	cartItemID := c.Param("id")
-	userID := c.Query("user_id") // atau dari JWT context kalau pakai auth
+	userID := c.Param("user_id") // atau dari JWT context kalau pakai auth
 
 	if err := services.DeleteCartItem(userID, cartItemID); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
